23c4e48413349869f9a0034a9bac3e243763eb55
Dipal Zambare <misomosispy@gmail.com>
2023-03-29 13:00:30 +0200

    Step by Step finished.

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -3,6 +3,9 @@ extends Sprite2D
 var speed = 400
 var angular_speed = PI
 
+signal player_died
+signal health_changed(old, new)
+
 func _ready():
 	var timer = get_node("Timer")
 	timer.timeout.connect(_on_timer_timeout)
@@ -14,6 +17,12 @@ func _process(delta):
 
 func _on_button_pressed():
 	set_process(not is_processing())
+	player_died.emit()
 
 func _on_timer_timeout():
 	visible = not visible
+	health_changed.emit(10, 20)
+
+
+func _on_health_changed(old, new):
+	print(old, new)
