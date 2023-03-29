1a6b4e31d557d7726278b96ce40f93dc52118b03
Dipal Zambare <misomosispy@gmail.com>
2023-03-29 12:43:57 +0200

    Signals via connect dialog

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -6,5 +6,8 @@ var angular_speed = PI
 func _process(delta):
 	rotation += angular_speed * delta
 	var velocity = Vector2.UP.rotated(rotation) * speed
-
 	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing())
